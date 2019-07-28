43a6ad92e013db862eb9e17ed14fa1ce5f607e15
Francesco Zoccheddu <zoccheddu.fra@gmail.com>
2019-07-28 20:20:11 +0200

    Minor update

--- animatorhelpers/src/main/java/com/francescozoccheddu/animatorhelpers/ABInt.kt
+++ animatorhelpers/src/main/java/com/francescozoccheddu/animatorhelpers/ABInt.kt
@@ -1,12 +1,9 @@
 package com.francescozoccheddu.animatorhelpers
 
-import kotlin.math.roundToInt
-
 class ABInt(initialValue: Int) : ABValue<Int>(initialValue) {
 
     override fun getDifference(a: Int, b: Int) = (a - b).toFloat()
 
-    override fun interpolate(from: Int, to: Int, progress: Float) =
-        lerp(from.toFloat(), to.toFloat(), progress).roundToInt()
+    override fun interpolate(from: Int, to: Int, progress: Float) = lerp(from, to, progress)
 
 }
